65a872a6f70b2db269ee4a256ea69325168345be
Babajide Fowotade <babajide.fowotade@andela.co>
2015-09-08 23:15:16 +0100

    add public folder bower components to list of ignored files

---
+++ server/public/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fccDaFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fccDaFrontEndApp
+ */
+angular.module('fccDaFrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ server/public/scripts/services/d3.js
@@ -0,0 +1,29 @@
+angular.module('fccDaFrontEndApp')
+  //.factory('dataService', ['$resource', function($rootScope, $resource){
+  //    $resource('/data');
+  //    //return {
+  //      //data: function(){return $resource('/data',{},{method:'GET', isArray:true});}
+  //      //data: function(){return $resource('/data', {}, {method:'GET', isArray:true})}
+  //    //}
+  //  }])
+  .factory('d3Service', ['$document', '$q', '$rootScope', '$window', function($document, $q, $rootScope, $window) {
+    return {
+      d3: function() {
+        var deffered = $q.defer();
+        function onScriptLoad() {
+          $rootScope.$apply(function(){ deffered.resolve($window.d3)});
+        }
+        var scriptTag = $document[0].createElement('script');
+        scriptTag.type = 'text/javascript';
+        scriptTag.async = true;
+        scriptTag.src = 'https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js';
+        scriptTag.onreadystatechange = function(){
+          if(this.readyState == 'complete') onScriptLoad();
+        }
+        scriptTag.onload = onScriptLoad;
+        var s = $document[0].getElementsByTagName('body')[0];
+        s.appendChild(scriptTag);
+        return deffered.promise;
+      }
+    }
+  }]);
