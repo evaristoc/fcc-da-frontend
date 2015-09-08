414fed90ecc6765d35adafbbd5ce0719a475129c
Babajide Fowotade <babajide.fowotade@andela.co>
2015-09-08 23:09:08 +0100

    changed views location

---
+++ server/public/scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fccDaFrontEndApp
+ * @description
+ * # fccDaFrontEndApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fccDaFrontEndApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      //.when('/data', {
+      //  templateUrl:'views/data.html'
+      //})
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
