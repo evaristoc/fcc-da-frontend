0744c998e78628c8e1c56d69e8753dc02d12321b
Babajide Fowotade <babajide.fowotade@andela.co>
2015-09-19 08:27:47 +0100

    remove conflict err code

--- server/app.server.js
+++ server/app.server.js
@@ -22,7 +22,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use(cookieParser());
-<<<<<<< HEAD
+
 app.use(logger('dev'));
 app.use(bodyParser.json({}));
 
@@ -35,26 +35,10 @@ app.use(bodyParser.urlencoded({
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
 
-||||||| merged common ancestors
-//app.use(express.static(path.join(__dirname, 'public')));
-// serve all asset files from necessary directories
-//http://stackoverflow.com/questions/20396900/angularjs-routing-in-expressjs
-//app.use("/styles", express.static('/home/ec/Public/freecodecamp/analytics_project/fcc_da_app/app/styles'));
-//app.use("/images", express.static('/home/ec/Public/freecodecamp/analytics_project/fcc_da_app/app/images'));
-//app.use("/scripts", express.static('/home/ec/Public/freecodecamp/analytics_project/fcc_da_app/app/scripts'));
-//app.use("/views", express.static('/home/ec/Public/freecodecamp/analytics_project/fcc_da_app/app/views'));
-app.set("view options", {layout: false});
-app.use(express.static(path.join(app.get('appPath'),'app')));
-app.use(express.static(path.join(app.get('appPath'),'bower_components')));
-//console.log(path.resolve(app.get('appPath') + '/index.html'))
-=======
-//app.use(express.static(path.join(__dirname, 'public')));
+
 // serve all asset files from necessary directories
 //http://stackoverflow.com/questions/20396900/angularjs-routing-in-expressjs
 app.set("view options", {layout: false});
-app.use(express.static(path.join(app.get('appPath'),'app')));
-app.use(express.static(path.join(app.get('appPath'),'bower_components')));
-//console.log(path.resolve(app.get('appPath') + '/index.html'))
 
 
 
@@ -103,5 +87,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-
