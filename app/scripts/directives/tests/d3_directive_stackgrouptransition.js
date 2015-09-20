5e1ad794ea818e81bacd9e2a74b6e3a0866ee6bb
ec <varikvi@yahoo.com>
2015-09-20 17:27:58 +0200

    Worked sketch stacked/grouped graph, two rooms (APP)

--- app/scripts/directives/tests/d3_directive_stackgrouptransition.js
+++ app/scripts/directives/tests/d3_directive_stackgrouptransition.js
@@ -97,8 +97,6 @@ angular.module('fccDaFrontEndApp')
           //https://javadude.wordpress.com/2012/06/18/d3-js-most-simple-stack-layout-with-bars/
           //http://bl.ocks.org/mbostock/1134768
           //http://bl.ocks.org/mbostock/3943967
-          //http://bl.ocks.org/mbostock/1166403
-          //http://bl.ocks.org/mbostock/3808218
           //http://www.d3noob.org/2012/12/adding-axis-labels-to-d3js-graph.html
           //ddata.unshift(dater);
           var margin = {
@@ -134,7 +132,7 @@ angular.module('fccDaFrontEndApp')
           //http://www.ireneros.com/conf/nicar/introduction-to-d3.html#9
           //http://stackoverflow.com/questions/31816637/d3js-error-while-drawing-the-chart
           //http://www.d3noob.org/2013/01/format-date-time-axis-with-specified.html
-          var format = d3.time.format("%d/%m/%Y");
+          var format = d3.time.format("%m/%Y");
           var x = d3.scale.ordinal().rangeRoundBands([0, width - margin.right - margin.left]);
           var xdate = d3.time.scale().range([0, width - margin.right - margin.left]);
           //xdate.domain(stacked[0].map(function(d) { console.log('in domain x', d); return d.x; }));
@@ -153,13 +151,9 @@ angular.module('fccDaFrontEndApp')
           var yGroupMax = d3.max(ddata, function(dd) { return d3.max(dd, function(d) { return d.y; }); });
           var yStackMax = d3.max(stacked, function(dd) { return d3.max(dd, function(d) { return d.y0 + d.y; }); });
           var y = d3.scale.linear().domain([0, yStackMax]).range([height, 0]);
-          //yAxis (axis()) is a function, not an object per se...
-          var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("right")
-            .ticks(5);
+          var yAxis = d3.svg.axis().scale(y)
+            .orient("right").ticks(5);
           
-          console.log(yGroupMax, yStackMax);
           
           //console.log("x.domain(): " + x.domain())
           //console.log("y.domain(): " + y.domain())
@@ -191,14 +185,13 @@ angular.module('fccDaFrontEndApp')
             .attr("transform", "translate(0," + height + ")")
             .call(xAxis);
           
-          //console.log(width - margin.left);
+          console.log(width - margin.left);
           var yaxis_pos = width - margin.left
           
           svg.append("g")
-            .attr("class", "yaxis")
+            .attr("class", "y axis")
             .attr("transform", "translate(" + yaxis_pos + ",0)")
             .call(yAxis);
-            //.remove();
 
           
           var legendRectSize = 20;
@@ -231,7 +224,33 @@ angular.module('fccDaFrontEndApp')
               .attr('y', legendRectSize - legendSpacing)
               .text(function(d) { return roomnames[d]; });
           
-          
+          //// Add a label per date.
+          //var label = svg.selectAll("text")
+          //  .data(x.domain())
+          //  .enter().append("svg:text")
+          //  .attr("x", function(d) { return x(d) + x.rangeBand() / 2; })
+          //  .attr("y", 6)
+          //  .attr("text-anchor", "middle")
+          //  .attr("dy", ".71em")
+          //  .text(format);
+          //
+          //// Add y-axis rules.
+          //var rule = svg.selectAll("g.rule")
+          //  .data(y.ticks(5))
+          //  .enter().append("svg:g")
+          //  .attr("class", "rule")
+          //  .attr("transform", function(d) { return "translate(0," + -y(d) + ")"; });
+          //
+          //rule.append("svg:line")
+          //  .attr("x2", width - p[1] - p[3])
+          //  .style("stroke", function(d) { return d ? "#fff" : "#000"; })
+          //  .style("stroke-opacity", function(d) { return d ? .7 : null; });
+          //
+          //rule.append("svg:text")
+          //  .attr("x", width)
+          //  .attr("dy", ".35em")
+          //  .text(d3.format(",d"));
+           
           d3.selectAll("input").on("change", change);
           
           var timeout = setTimeout(function() {
@@ -245,21 +264,9 @@ angular.module('fccDaFrontEndApp')
             else transitionStacked();
           };
 
-          
-        function rescale(yrange) {
-            svg.select(".yaxis")
-                    .transition().duration(1500).ease("sin-in-out")  // https://github.com/mbostock/d3/wiki/Transitions#wiki-d3_ease
-                    .call(yrange);  
-            svg.select(".yaxis label")
-                .text("Rescaled Axis");
-        }          
-          
         function transitionGrouped() {
           y.domain([0, yGroupMax]);
-          console.log(1, yAxis);
-          yAxis.scale(y);
-          console.log(1, yAxis);
-          console.log(1, svg.selectAll(".yaxis"));
+        
           rect.transition()
             .duration(500)
             .delay(function(d, i) { return i * 10; })
@@ -268,20 +275,11 @@ angular.module('fccDaFrontEndApp')
             .transition()
             .attr("y", function(d) { return y(d.y); })
             .attr("height", function(d) { return height - y(d.y); });
-          
-          rescale(yAxis);
         }
         
         function transitionStacked() {
           y.domain([0, yStackMax]);
-          console.log(2, yAxis);
-          yAxis.scale(y);
-          console.log(2, yAxis);
-          console.log(2, svg.selectAll(".yaxis"));
-          console.log(2, d3.select(".yaxis"));
-          svg.select(".y axis").remove();
-          console.log(2, svg.selectAll(".yaxis"));
-          console.log(2, d3.select(".yaxis"));
+        
           rect.transition()
             .duration(500)
             .delay(function(d, i) { return i * 10; })
@@ -290,12 +288,188 @@ angular.module('fccDaFrontEndApp')
             .transition()
             .attr("x", function(d) { return x(d.x); })
             .attr("width", x.rangeBand());
-          
-          rescale(yAxis);
-          
         }
-      });
-    });
-   }
-  }
-}]);
+
+
+        
+        //rule.append("svg:line")
+        //  .attr("x2", width - p[1] - p[3])
+        //  .style("stroke", function(d) { return d ? "#fff" : "#000"; })
+        //  .style("stroke-opacity", function(d) { return d ? .7 : null; });
+        //
+        //rule.append("svg:text")
+        //  .attr("x", width - p[1] - p[3] + 6)
+        //  .attr("dy", ".35em")
+        //  .text(d3.format(",d"));
+           
+           
+//////////////////////////////////////////////////////          
+          
+         //var margin = {
+         //     top: 30,
+         //     right: 20,
+         //     bottom: 30,
+         //     left: 50
+         //   },
+         // //width = 600 - margin.left - margin.right,
+         // //height = 270 - margin.top - margin.bottom;
+         // 
+         // width = 780
+         // height = 500
+         // p = [20, 50, 30, 50]
+         // 
+         // var svg = d3.select(elem[0])
+         //   .append("svg")
+         //   .attr("width", width + margin.left + margin.right)
+         //   .attr("height", height + margin.top + margin.bottom)
+         //   .append("g")
+         //   .attr("transform",
+         //     "translate(" + p[3] + "," + (height - p[2]) + ")");          
+         //   //.append("svg:svg")
+         //   //.attr("class", "chart")
+         //   //.attr("width", width)
+         //   //.attr("height", height )
+         //   //.append("svg:g")
+         //   //.attr("transform", "translate(10,470)");
+         //
+         // x = d3.scale.ordinal().rangeRoundBands([0, width - p[1] - p[3]]);
+         // y = d3.scale.linear().range([0, height - p[0] - p[2]])
+         // //z = d3.scale.ordinal().range(["darkblue", "blue", "lightblue"])
+         // z = d3.scale.ordinal().range(["darkblue", "blue"])
+         // //http://schoolofdata.org/2013/08/12/climbing-the-d3-js-visualisation-stack/
+         // //data = [];
+         // //for (var r = 0; r < dater.length; r++){
+         // //  for (var c = 1; c < ddata.length; c++) {
+         // //    data.push({x: ddata[0][r], y: ddata[c][r] })
+         // //  }
+         // //}
+         //       
+         // var stacked = d3.layout.stack()(ddata);
+         // 
+         // //console.log(stacked);
+         // 
+         // x.domain(stacked[0].map(function(d) { return d.x; }));
+         // y.domain([0, d3.max(stacked[stacked.length - 1], function(d) { return d.y0 + d.y; })]);
+         // 
+         // //console.log("x.domain(): " + x.domain())
+         // //console.log("y.domain(): " + y.domain())
+         // //console.log("------------------------------------------------------------------");
+         //
+         // var valgroup = svg.selectAll("g.valgroup")
+         //   .data(stacked)
+         //   .enter().append("svg:g")
+         //   .attr("class", "valgroup")
+         //   .style("fill", function(d, i) { return z(i); })
+         //   .style("stroke", function(d, i) { return d3.rgb(z(i)).darker(); });          
+         // 
+         // var rect = valgroup.selectAll("rect")
+         //  .data(function(d){return d;})
+         //  .enter().append("svg:rect")
+         //  .attr("x", function(d) { return x(d.x); })
+         //  .attr("y", function(d) { return -y(d.y0) - y(d.y); })
+         //  .attr("height", function(d) { return y(d.y); })
+         //  .attr("width", x.rangeBand());
+         //  
+         // // Add y-axis rules.
+         // var rule = svg.selectAll("g.rule")
+         //   .data(y.ticks(5))
+         //   .enter().append("svg:g")
+         //   .attr("class", "rule")
+         //   .attr("transform", function(d) { return "translate(0," + -y(d) + ")"; });
+         //
+         // rule.append("svg:line")
+         //   .attr("x2", width - p[1] - p[3])
+         //   .style("stroke", function(d) { return d ? "#fff" : "#000"; })
+         //   .style("stroke-opacity", function(d) { return d ? .7 : null; });
+         //
+         // rule.append("svg:text")
+         //   .attr("x", width - p[1] - p[3] + 6)
+         //   .attr("dy", ".35em")
+         //   .text(d3.format(",d"));
+          
+//////////////////////////////////////////////////////////////////////////////          
+          //data = data[0];
+          //var minDate = predata[0][0].day,
+          //  maxDate = data[data.length - 1].day;          
+          //console.log(minDate, maxDate);
+          ////var x = d3.scale.linear().range([0, width]);
+          //var x = d3.time.scale().range([0, width]);
+          //var y = d3.scale.linear().range([height, 0]);
+          //
+          //// Define the axes
+          //var xAxis = d3.svg.axis().scale(x)
+          //  .orient("bottom").ticks(5);
+          //
+          //var yAxis = d3.svg.axis().scale(y)
+          //  .orient("left").ticks(5);
+          //
+          //// Define the line
+          //var valueline = d3.svg.line()
+          //  .x(function(d) {
+          //    return x(d.day);
+          //  })
+          //  .y(function(d) {
+          //    return y(d.hum);
+          //  });
+          //
+          //// Adds the svg canvas
+          //var svg = d3.select(elem[0]) //select where directive was added
+          //  .append("svg")
+          //  .attr("width", width + margin.left + margin.right)
+          //  .attr("height", height + margin.top + margin.bottom)
+          //  .append("g")
+          //  .attr("transform",
+          //    "translate(" + margin.left + "," + margin.top + ")");
+          //
+          //var h = [];
+          //var useddata = [];
+          //for (var kkkk = 0; kkkk < data.length; kkkk++) {
+          //  console.log(minDate, data[kkkk].day)
+          //  if (minDate == data[kkkk].day) {
+          //    var slicdata = data.indexOf(data[kkkk]);
+          //    //for (kkkk; kkkk < data.length; kkkk++) {
+          //    //  useddata.push(data[kkkk])
+          //    //}
+          //    var useddata = data.slice(slicdata, (data.length-1));
+          //    break;
+          //  }
+          //};
+          //console.log(useddata);
+          //
+          //useddata.forEach(function(e, i, a) {
+          //  //d = parseDate(e.day);
+          //  console.log(e.day);
+          //  h.push(e.hum);
+          //});
+          //
+          //// Scale the range of the data
+          //
+          //x.domain([minDate, maxDate]).range([0, width]);
+          //y.domain([0, d3.max(h, function(d) {
+          //  return d;
+          //})]);
+          //
+          //// Add the valueline path.
+          //svg.append("path")
+          //  .attr("class", "line")
+          //  .attr("d", valueline(useddata));
+          //
+          //// Add the X Axis
+          //svg.append("g")
+          //  .attr("class", "x axis")
+          //  .attr("transform", "translate(0," + height + ")")
+          //  .call(xAxis);
+          //
+          //// Add the Y Axis
+          //svg.append("g")
+          //  .attr("class", "y axis")
+          //  .call(yAxis);
+          //}, function(error) {
+          //// error handler
+          //console.log("$resouce call got an ERROR");
+          });
+
+        });
+      }
+    }
+  }]);
